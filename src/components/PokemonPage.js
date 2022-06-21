5c7abed2a1ca5740497e9a305d0ac667df51f201
LGmedia954 <LeslieGonzalez@me.com>
2022-06-21 15:49:12 -0400

    commit tree 1ee3f5e7f1205e2dcabceb27a72c2ec438eda637

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,23 +1,64 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import Search from './Search'
-import { Container } from 'semantic-ui-react'
+// import React from 'react'
+// import PokemonCollection from './PokemonCollection'
+// import PokemonForm from './PokemonForm'
+// <<<<<<< HEAD
+// import Search from './Search'
+// import { Container } from 'semantic-ui-react'
+// =======
+// import { Search } from 'semantic-ui-react'
+// import _ from 'lodash'
 
-class PokemonPage extends React.Component {
-  render() {
-    return (
-      <Container>
-        <h1>Pokemon Searcher</h1>
-        <br />
-        <PokemonForm />
-        <br />
-        <Search />
-        <br />
-        <PokemonCollection />
-      </Container>
-    )
-  }
-}
+// class PokemonIndex extends React.Component {
+//   state = {
+//     pokemonCollection: [],
+//     searchTerm: ''
+//   }
 
-export default PokemonPage
+//   componentDidMount() {
+//     fetch('http://localhost:3000/pokemon')
+//       .then(res => res.json())
+//       .then(pokemonCollection => this.setState({ pokemonCollection: pokemonCollection }))
+//       .catch(e => console.error(e))
+//   }
+
+//   handleSearchChange = (e, { value }) => {
+//     this.setState({ searchTerm: value })
+//   }
+
+//   toggleImage = pokemon => {
+//     const col = this.state.pokemonCollection
+//     const i = col.indexOf(pokemon)
+//     this.setState({
+//       pokemonCollection: [
+//         ...col.slice(0, i),
+//         // initially pokemon.isClicked is undefined; inverting that falsey value makes it true
+//         { ...pokemon, isClicked: !pokemon.isClicked },
+//         ...col.slice(i + 1)
+//       ]
+//     })
+//   }
+
+//   addPokemon = pokemon => {
+//     this.setState({ pokemonCollection: [...this.state.pokemonCollection, pokemon] })
+//   }
+// >>>>>>> Update PokemonIndex.js
+
+
+
+// class PokemonPage extends React.Component {
+//   render() {
+//     return (
+//       <Container>
+//         <h1>Pokemon Searcher</h1>
+//         <br />
+//         <PokemonForm />
+//         <br />
+//         <Search onChange={() => console.log('🤔')} />
+//         <br />
+//         <PokemonCollection />
+//       </Container>
+//     )
+//   }
+// }
+
+// export default PokemonPage
